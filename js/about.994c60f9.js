0b8a1f1deabe1405e636c25159aee4ead83ceafc
Jonny Whittle <jonny@builtbyplatform.co.uk>
2024-12-10 22:29:30 +0000

    Updates

--- js/about.994c60f9.js
+++ js/about.994c60f9.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkchristmas_cocktail_app"]=self["webpackChunkchristmas_cocktail_app"]||[]).push([[594],{5609:function(a,t,c){c.r(t),c.d(t,{default:function(){return o}});var n=c(641);const s={class:"about"};function u(a,t){return(0,n.uX)(),(0,n.CE)("div",s,t[0]||(t[0]=[(0,n.Lk)("h1",null,"This is an about page",-1)]))}var r=c(6262);const e={},i=(0,r.A)(e,[["render",u]]);var o=i}}]);
-//# sourceMappingURL=about.d599aa82.js.map
+//# sourceMappingURL=about.994c60f9.js.map
